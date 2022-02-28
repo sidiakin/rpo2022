88525a3f0beafa9980833974d7091e486d49809a
Ivan Sidyakin <ivan.sidyakin@gmail.com>
2022-02-28 19:24:16 +0300

    step 4. PinpadActivity

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,7 +1,13 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,16 +18,34 @@ import org.apache.commons.codec.binary.Hex;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityResultLauncher activityResultLauncher;
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
         int res = initRng();
         byte[] rnd = randomBytes(10);
@@ -31,14 +55,17 @@ public class MainActivity extends AppCompatActivity {
     {
         //Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
 
-        byte[] key =
-                stringToHex("0123456789ABCDEF0123456789ABCDE0");
-        byte[] enc = encrypt(key,
-                stringToHex("000000000000000102"));
-        byte[] dec = decrypt(key, enc);
-        String s = new String(Hex.encodeHex(dec)).toUpperCase();
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+        //byte[] key =
+        //        stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        //byte[] enc = encrypt(key,
+        //        stringToHex("000000000000000102"));
+        //byte[] dec = decrypt(key, enc);
+        //String s = new String(Hex.encodeHex(dec)).toUpperCase();
+        //Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
 
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
     }
 
 
